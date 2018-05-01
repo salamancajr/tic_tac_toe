c425fd07255fb4a7cc65e43d96f66299aa1f4b2a
George Salamanca Jr <georgesalamancajr@Georges-Mac-mini.local>
2018-04-30 21:47:24 -0400

    tic_tac_toe

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000;
+ app.use(express.static(__dirname+"/"))
 app.get("/", (req, res)=>{
-    res.sendFile("/index.html")
+    res.sendFile(__dirname+"/index.html")
 })
 app.listen(port)
